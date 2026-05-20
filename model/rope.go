df29aa6bc97835ada63431e097435022b24d5393
Rui Carmo <rui.carmo@gmail.com>
2026-05-20 13:04:50 +0100

    Move RoPE frequency generation to SIMD

--- model/rope.go
+++ model/rope.go
@@ -1,10 +1,6 @@
 package model
 
-import (
-	"math"
-
-	simd "github.com/rcarmo/go-pherence/backends/simd/runtime"
-)
+import simd "github.com/rcarmo/go-pherence/backends/simd/runtime"
 
 func (m *LlamaModel) precomputeRoPE() {
 	cfg := m.Config
@@ -26,39 +22,11 @@ func (m *LlamaModel) precomputeRoPE() {
 }
 
 func buildRoPEFreqs(maxSeq, halfDim, headDim int, theta float64) []float32 {
-	n, ok := checkedRoPEFreqLen(maxSeq, halfDim)
-	if !ok || headDim <= 0 {
-		return nil
-	}
-	if theta <= 0 {
-		theta = 10000
-	}
-	freqs := make([]float32, n)
-	for pos := 0; pos < maxSeq; pos++ {
-		for i := 0; i < halfDim; i++ {
-			freq := 1.0 / math.Pow(theta, float64(2*i)/float64(headDim))
-			angle := float64(pos) * freq
-			off := (pos*halfDim + i) * 2
-			freqs[off] = float32(math.Cos(angle))
-			freqs[off+1] = float32(math.Sin(angle))
-		}
-	}
-	return freqs
+	return simd.BuildRoPEFreqs(maxSeq, halfDim, headDim, theta)
 }
 
 func checkedRoPEFreqLen(maxSeq, halfDim int) (int, bool) {
-	if maxSeq <= 0 || halfDim <= 0 {
-		return 0, false
-	}
-	maxInt := int(^uint(0) >> 1)
-	if maxSeq > maxInt/halfDim {
-		return 0, false
-	}
-	n := maxSeq * halfDim
-	if n > maxInt/2 {
-		return 0, false
-	}
-	return n * 2, true
+	return simd.CheckedRoPEFreqLen(maxSeq, halfDim)
 }
 
 func applyRoPE(x, freqs []float32, pos, numHeads, headDim int) {
