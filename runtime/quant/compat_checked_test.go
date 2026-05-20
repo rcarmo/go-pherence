2d0597d2ff6a7de29853738ca319e6d11db9da5a
Rui Carmo <rui.carmo@gmail.com>
2026-05-20 07:48:04 +0100

    Test checked quant compatibility wrappers

---
+++ runtime/quant/compat_checked_test.go
@@ -0,0 +1,59 @@
+package quant
+
+import "testing"
+
+func TestCheckedCompatibilityWrappersRejectMalformedInputs(t *testing.T) {
+	if GemvQ4To(make([]float32, 1), make([]float32, 1), nil, nil, nil, nil, 8, 8, true) {
+		t.Fatal("GemvQ4To accepted malformed input")
+	}
+	if GemvQ4SymTo(make([]float32, 1), make([]float32, 1), nil, nil, nil, 8, 8) {
+		t.Fatal("GemvQ4SymTo accepted malformed input")
+	}
+	if GemvMLQTo(make([]float32, 1), make([]float32, 1), nil) {
+		t.Fatal("GemvMLQTo accepted malformed input")
+	}
+	if GemvNVFP4To(make([]float32, 1), make([]float32, 1), nil) {
+		t.Fatal("GemvNVFP4To accepted malformed input")
+	}
+}
+
+func TestCheckedCompatibilityWrappersAcceptTinyValidInputs(t *testing.T) {
+	q4Out := make([]float32, 1)
+	q4X := []float32{1, 2, 3, 4, 5, 6, 7, 8}
+	q4Weight := []int32{0x76543210}
+	q4GIdx := make([]int32, 8)
+	q4Scales := []float32{0.5}
+	if !GemvQ4SymTo(q4Out, q4X, q4Weight, q4GIdx, q4Scales, 8, 1) {
+		t.Fatal("GemvQ4SymTo rejected valid tiny input")
+	}
+	if !GemvQ4To(q4Out, q4X, q4Weight, nil, q4GIdx, q4Scales, 8, 1, true) {
+		t.Fatal("GemvQ4To rejected valid tiny symmetric input")
+	}
+
+	mlxWeight := &MLXQuantWeight{
+		Weight:    []uint32{0x76543210},
+		Scales:    []float32{0.5},
+		Biases:    []float32{0},
+		OutDim:    1,
+		InDim:     8,
+		Groups:    1,
+		GroupSize: 8,
+		Bits:      4,
+	}
+	if !GemvMLQTo(make([]float32, 1), q4X, mlxWeight) {
+		t.Fatal("GemvMLQTo rejected valid tiny input")
+	}
+
+	nvWeight := &NVFP4Weight{
+		Weight:       []byte{0x10, 0x32, 0x54, 0x76},
+		WeightScale:  []byte{0x38},
+		WeightScale2: 1,
+		OutDim:       1,
+		InDim:        8,
+		Groups:       1,
+		GroupSize:    8,
+	}
+	if !GemvNVFP4To(make([]float32, 1), q4X, nvWeight) {
+		t.Fatal("GemvNVFP4To rejected valid tiny input")
+	}
+}
