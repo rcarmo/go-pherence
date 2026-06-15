91721c93415dc2bdb486bc1c24d6be6a2d751929
Rui Carmo <rui.carmo@gmail.com>
2026-06-15 13:45:16 +0100

    test: mark compact Q4 expert parity gate

---
+++ backends/nvidia/runtime/q4k_compact_test.go
@@ -0,0 +1,7 @@
+package nvidia
+
+import "testing"
+
+func TestGateUpQ4KByWorkPtrsCompactScaleParity(t *testing.T) {
+	t.Skip("TODO: implement compact Q4_K resident scale/min pointer-table kernel; must match F32-scale kernel within DiffusionGemma GGUF expert tolerance before enabling")
+}
