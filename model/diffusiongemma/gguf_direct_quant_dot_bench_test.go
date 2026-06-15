754e31d5f7d24b16909817f8aef46b5a511c9ae5
Rui Carmo <rui.carmo@gmail.com>
2026-06-15 08:19:50 +0100

    bench: cover q8 direct row dot crossover

--- model/diffusiongemma/gguf_direct_quant_dot_bench_test.go
+++ model/diffusiongemma/gguf_direct_quant_dot_bench_test.go
@@ -90,7 +90,7 @@ func BenchmarkGGUFQ8_0RowDotDirectVsDequant(b *testing.B) {
 func BenchmarkGGUFQ8_0RowDotBatchDirectVsDequant(b *testing.B) {
 	m := syntheticBenchQ8Matrix(b, 2816)
 	row := make([]float32, m.InDim)
-	for _, nPos := range []int{1, 2, 3, 4, 8} {
+	for _, nPos := range []int{1, 2, 3, 4, 8, 16} {
 		x := make([]float32, nPos*m.InDim)
 		directOut := make([]float32, nPos*m.OutDim)
 		dequantOut := make([]float32, nPos*m.OutDim)
