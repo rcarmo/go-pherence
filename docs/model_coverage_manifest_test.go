249fa6883bb3cb03fc75d2d177b32fe7b07d81ee
Rui Carmo <rui.carmo@gmail.com>
2026-06-22 21:22:58 +0100

    docs: track MiniCPM-V pending runtime steps

--- docs/model_coverage_manifest_test.go
+++ docs/model_coverage_manifest_test.go
@@ -143,6 +143,7 @@ func TestModelCoverageManifest(t *testing.T) {
 		"runtime_readiness_report":     "../model/minicpmv/readiness.go",
 		"capability_summary":           "../model/minicpmv/capabilities.go",
 		"runtime_status_constant":      "../model/minicpmv/capabilities.go",
+		"pending_runtime_steps":        "../model/minicpmv/runtime_status.go",
 		"metadata_fixture":             "../model/minicpmv/testdata/minicpmo_fixture/config.json",
 		"fixture_path_constant":        "../model/minicpmv/fixtures.go",
 		"fixture_check_make_target":    "../Makefile",
