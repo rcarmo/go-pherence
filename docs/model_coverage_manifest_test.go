c0f83c6e51489858a14b4d4e25ffd3a1ceaee215
Rui Carmo <rui.carmo@gmail.com>
2026-06-22 22:01:59 +0100

    docs: track MiniCPM-O fixture summary helper

--- docs/model_coverage_manifest_test.go
+++ docs/model_coverage_manifest_test.go
@@ -151,6 +151,7 @@ func TestModelCoverageManifest(t *testing.T) {
 		"pending_runtime_steps":              "../model/minicpmv/runtime_status.go",
 		"metadata_fixture":                   "../model/minicpmv/testdata/minicpmo_fixture/config.json",
 		"metadata_expected_summary":          "../model/minicpmv/testdata/minicpmo_fixture/expected_summary.json",
+		"fixture_expected_summary_helper":    "../model/minicpmv/fixture_summary.go",
 		"fixture_path_constant":              "../model/minicpmv/fixtures.go",
 		"fixture_metadata_helper":            "../model/minicpmv/fixtures.go",
 		"fixture_path_make_target":           "../Makefile",
