e41a63f9c784e4aafec7fd89310130b27775082d
Rui Carmo <rui.carmo@gmail.com>
2026-06-22 23:47:18 +0100

    docs: track MiniCPM-V support roadmap path

--- docs/model_coverage_manifest_test.go
+++ docs/model_coverage_manifest_test.go
@@ -148,6 +148,7 @@ func TestModelCoverageManifest(t *testing.T) {
 		"runtime_readiness_report":              "../model/minicpmv/readiness.go",
 		"capability_summary":                    "../model/minicpmv/capabilities.go",
 		"support_summary_api":                   "../model/minicpmv/support_summary.go",
+		"support_summary_roadmap_path":          "../model/minicpmv/support_summary.go",
 		"support_summary_validation":            "../model/minicpmv/support_validation.go",
 		"support_summary_inspect_mode":          "../cmd/minicpmvinspect/main.go",
 		"support_summary_make_target":           "../Makefile",
